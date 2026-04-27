335ba498a3ac522737fce76ef9329c973a453594
Arav Jain <arav918@gmail.com>
2026-04-27 17:42:45 +0530

    Add Constants.java

---
+++ app/src/main/java/com/urbangaze/app/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.urbangaze.app.utils;
+
+public class Constants {
+    public static final String OLA_API_KEY = "";
+    public static final String SUPABASE_URL = "";
+
+    public static final String SUPABASE_API_KEY = "";
+    public static final String BUCKET_NAME = "trip_images";
+
+}
